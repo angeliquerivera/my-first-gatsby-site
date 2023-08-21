ef911021cff8b92c88062caeca2a17b31feef8c7
Angelique Rivera <angeliqueriveramorales@gmail.com>
2023-08-21 14:51:05 -0400

    feat(GraphiQL): add query data in layout and gatsby-config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,20 @@
 // Step 1: Import React
 import * as React from "react";
-import { Link } from "gatsby";
 import Layout from "../components/layout";
-// Step 2: Define your component
+import { StaticImage } from "gatsby-plugin-image";
+
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Vintage style image of Samus Aran from the Metroid Prime video game series"
+        src="../images/bombshellSamus.jpg"
+      />
     </Layout>
   );
 };
 
-// You'll learn about this in the next task, just copy it for now
 export const Head = () => <title>Home Page</title>;
 
-// Step 3: Export your component
 export default IndexPage;
